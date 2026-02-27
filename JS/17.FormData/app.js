75fcc450ee48418a75f64b3bee8eaa160d16f6f4
adilmalik01 <malikadil5679@gmail.com>
2026-02-27 16:02:53 +0500

    update : FormD data

---
+++ JS/17.FormData/app.js
@@ -0,0 +1,25 @@
+
+let submitBtn = document.querySelector("#submit");
+let passInput = document.querySelector("#password");
+let emailInput = document.querySelector("#email");
+
+
+
+submitBtn.addEventListener("click",()=>{
+    console.log(passInput.value);
+    console.log(emailInput.value);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
